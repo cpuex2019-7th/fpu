82fe66585d626ef57ed304b130a1dd83cfaf1d9f
hashi0203 <n.hsmt1123@gmail.com>
2019-11-12 22:12:47 +0900

    fdiv fsqrt updated

--- fsqrt/exec.sh
+++ fsqrt/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/.bash_profile
-xvlog --sv test_fsqrt.sv fsqrt.sv
+xvlog --sv test_fsqrt.sv fsqrt.sv ../fmul/fmul.sv
 xelab -debug typical test_fsqrt -s test_fsqrt.sim
 xsim --runall test_fsqrt.sim
